3d54911bdf294021a49eb7798f5d3660ef0d3aee
CoreyBabs <coreymbabcock@gmail.com>
2023-07-02 11:52:48 -0600

    Add solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+  if (val1 < 0 || val2 < 0 || !Number.isInteger(val1) || !Number.isInteger(val2)) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  let max = val1 > val2 ? val1 : val2;
+  let min = val1 < val2 ? val1 : val2;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  
+  return sum;
 };
 
 // Do not edit below this line
