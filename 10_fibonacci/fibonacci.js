41be82ca6be528a5fd1f9c293974e47024f52a62
CoreyBabs <coreymbabcock@gmail.com>
2023-07-08 11:30:27 -0600

    add exercise 10 solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
+  if (+idx < 0) return "OOPS";
+  if (+idx === 1 || +idx === 2) {
+    return 1;
+  }
 
+  return fibonacci(idx - 1) + fibonacci(idx - 2);
 };
 
 // Do not edit below this line
