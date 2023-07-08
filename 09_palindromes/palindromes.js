8ecb4dfa786d00ede2ad29d49c341a4648220313
CoreyBabs <coreymbabcock@gmail.com>
2023-07-08 11:20:21 -0600

    add soliton to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let t = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  t = t.replace(/\s/g, "");
+  t = t.toLowerCase();
+  let reversed = t.split("").reverse().join("");
+  return reversed === t;
 };
 
 // Do not edit below this line
