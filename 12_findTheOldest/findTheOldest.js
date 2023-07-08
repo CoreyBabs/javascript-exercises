b347f4d14a866c9f0ead1c58ed728357eee7bf75
CoreyBabs <coreymbabcock@gmail.com>
2023-07-08 11:44:59 -0600

    add exercise 11 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    let oldestDeath = "yearOfDeath" in oldest ? oldest["yearOfDeath"] : new Date().getFullYear();
+    let personDeath = "yearOfDeath" in person ? person["yearOfDeath"] : new Date().getFullYear();
+    let oldestAge = oldestDeath - oldest["yearOfBirth"];
+    let personAge = personDeath - person["yearOfBirth"];
+    return oldestAge > personAge ? oldest : person;
+  }, people[0]);
 };
 
 // Do not edit below this line
